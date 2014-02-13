362c6a694e01d483cd1d9285276ab98d883aa506
Miko Mynttinen <miko.mynttinen@helsinki.fi>
2014-02-13 20:13:32 +0200

    unit testit kirjoitettu

--- src/test/java/fi/miko/tiralabra/ShortestPathTest.java
+++ src/test/java/fi/miko/tiralabra/ShortestPathTest.java
@@ -1,6 +1,7 @@
 package fi.miko.tiralabra;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Random;
 
@@ -18,28 +19,9 @@ import fi.miko.tiralabra.datastructures.LinkedList;
 public class ShortestPathTest {
 	private static final double SQRT2 = Math.sqrt(2);
 
-	private void testPathFinder(PathFinder f, double length) {
-		f.findPath();
-
-		LinkedList<Node> path = f.getShortestPath();
-
-		if (length > 0) {
-			assertTrue(isValidPath(f.getGraph(), path));
-			assertEquals(length, getPathDistance(path), 1E-9);
-		} else {
-			assertTrue(path.isEmpty());
-		}
-	}
-
-	private void testPathFinders(char[][] map, double length) {
-		testPathFinder(new BellmanFord(new Graph(map)), length);
-		testPathFinder(new Dijkstra(new Graph(map)), length);
-		testPathFinder(new AStar(new Graph(map), Heuristic.Euclidean), length);
-	}
-
 	// @formatter:off
 	@Test
-	public void testTwoNodes() {
+	public void test1x1() {
 		testPathFinders(new char[][] {
 				{ 's', 't' } }, 1);
 
@@ -56,13 +38,7 @@ public class ShortestPathTest {
 	}
 
 	@Test
-	public void testThreeNodes() {
-		testPathFinders(new char[][] {
-				{ 's', 'p', 't' } }, 2);
-
-		testPathFinders(new char[][] {
-				{ 't', 'p', 's' } }, 2);
-
+	public void test1x3() {
 		testPathFinders(new char[][] {
 				{ 's' },
 				{ 'p' },
@@ -72,15 +48,6 @@ public class ShortestPathTest {
 				{ 't' },
 				{ 'p' },
 				{ 's' } }, 2);
-	}
-
-	@Test
-	public void testThreeNodesWall() {
-		testPathFinders(new char[][] {
-				{ 's', '#', 't' } }, -1);
-
-		testPathFinders(new char[][] {
-				{'t', '#', 's' } }, -1);
 
 		testPathFinders(new char[][] {
 				{ 's' },
@@ -129,7 +96,60 @@ public class ShortestPathTest {
 	}
 
 	@Test
+	public void test2x3() {
+		testPathFinders(new char[][] {
+				{ 's', 'n' },
+				{ '#', '#' },
+				{ 'n', 't' } }, -1);
+
+		testPathFinders(new char[][] {
+				{ 't', 'n' },
+				{ '#', '#' },
+				{ 'n', 's' } }, -1);
+
+		testPathFinders(new char[][] {
+				{ 's', 'n' },
+				{ '#', 'p' },
+				{ 'n', 't' } }, SQRT2 + 1);
+
+		testPathFinders(new char[][] {
+				{ 's', 'n' },
+				{ 'p', '#' },
+				{ 'n', 't' } }, SQRT2 + 1);
+	}
+
+	@Test
+	public void test3x1() {
+		testPathFinders(new char[][] {
+				{ 's', 'p', 't' } }, 2);
+
+		testPathFinders(new char[][] {
+				{ 't', 'p', 's' } }, 2);
+
+		testPathFinders(new char[][] {
+				{ 's', 'p', 't' } }, 2);
+
+		testPathFinders(new char[][] {
+				{ 't', 'p', 's' } }, 2);
+
+		testPathFinders(new char[][] {
+				{ 's', '#', 't' } }, -1);
+
+		testPathFinders(new char[][] {
+				{'t', '#', 's' } }, -1);
+
+	}
+
+	@Test
 	public void test3x2() {
+		testPathFinders(new char[][] {
+				{ 's', '#', 'n'},
+				{ 'n', '#', 't'} }, -1);
+
+		testPathFinders(new char[][] {
+				{ 't', '#', 'n'},
+				{ 'n', '#', 's'} }, -1);
+
 		testPathFinders(new char[][] {
 				{ 's', '#', 'n'},
 				{ 'n', 'p', 't'} }, SQRT2 + 1);
@@ -140,16 +160,27 @@ public class ShortestPathTest {
 	}
 
 	@Test
-	public void test2x3() {
+	public void test3x3() {
 		testPathFinders(new char[][] {
-				{ 's', 'n' },
-				{ '#', 'p' },
-				{ 'n', 't' } }, SQRT2 + 1);
+				{ 's', '#', 'n'},
+				{ '#', '#', 'n'},
+				{ 'n', 'n', 't'} }, -1);
 
 		testPathFinders(new char[][] {
-				{ 's', 'n' },
-				{ 'p', '#' },
-				{ 'n', 't' } }, SQRT2 + 1);
+				{ 's', 'n', 'n'},
+				{ 'n', '#', '#'},
+				{ 'n', '#', 't'} }, -1);
+
+		testPathFinders(new char[][] {
+				{ 's', '#', 'n'},
+				{ '#', 'p', 'n'},
+				{ 'n', 'n', 't'} }, SQRT2 * 2);
+
+		testPathFinders(new char[][] {
+				{ 's', 'p', 'n'},
+				{ '#', '#', 'p'},
+				{ 'n', 'n', 't'} }, SQRT2 + 2);
+
 	}
 
 	@Test
@@ -191,7 +222,95 @@ public class ShortestPathTest {
 				{ 'n', 'p', 'p', 'n'} }, SQRT2 * 2 + 5);
 	}
 
+	@Test
+	public void testConclave() {
+		testPathFinders(new char[][] {
+				{ 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n'},
+				{ 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n'},
+				{ 'n', 'n', '#', '#', '#', 'n', 'n', 'n'},
+				{ 'n', 'n', 'n', 'n', '#', 'n', 'n', 'n'},
+				{ 's', 'n', 'n', 'n', '#', 't', 'n', 'n'},
+				{ 'n', 'p', '#', '#', '#', 'p', 'n', 'n'},
+				{ 'n', 'n', 'p', 'p', 'p', 'n', 'n', 'n'},
+				{ 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n'} }, SQRT2 * 3 + 3);
+
+		testPathFinders(new char[][] {
+				{ 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n'},
+				{ 'n', 'n', 'p', 'p', 'p', 'n', 'n', 'n'},
+				{ 'n', 'p', '#', '#', '#', 'p', 'n', 'n'},
+				{ 's', 'n', 'n', 'n', '#', 't', 'n', 'n'},
+				{ 'n', 'n', 'n', 'n', '#', 'n', 'n', 'n'},
+				{ 'n', 'n', '#', '#', '#', 'n', 'n', 'n'},
+				{ 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n'},
+				{ 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n'} }, SQRT2 * 3 + 3);
+
+		testPathFinders(new char[][] {
+				{ 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n'},
+				{ 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n'},
+				{ 'n', 'n', '#', '#', '#', 'n', 'n', 'n'},
+				{ 'n', 'n', 'n', 'n', '#', 'n', 'n', 'n'},
+				{ 'n', 'n', 'p', 's', '#', 't', 'n', 'n'},
+				{ 'n', 'p', '#', '#', '#', 'p', 'n', 'n'},
+				{ 'n', 'n', 'p', 'p', 'p', 'n', 'n', 'n'},
+				{ 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n'} }, SQRT2 * 3 + 4);
+
+		testPathFinders(new char[][] {
+				{ 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n'},
+				{ 'n', 'n', 'p', 'p', 'p', 'n', 'n', 'n'},
+				{ 'n', 'p', '#', '#', '#', 'p', 'n', 'n'},
+				{ 'n', 'n', 'p', 'n', '#', 't', 'n', 'n'},
+				{ 'n', 'n', 'n', 's', '#', 'n', 'n', 'n'},
+				{ 'n', 'n', '#', '#', '#', 'n', 'n', 'n'},
+				{ 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n'},
+				{ 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n'} }, SQRT2 * 4 + 3);
+	}
+
 	// @formatter:on
+
+	private void testPathFinder(PathFinder f, double length) {
+		LinkedList<Node> path = getShortestPath(f);
+
+		if (length > 0) {
+			assertEquals(length, getPathDistance(path), 1E-9);
+		} else {
+			assertTrue(path.isEmpty());
+		}
+	}
+
+	private void testPathFinders(char[][] map, double length) {
+		testPathFinder(new BellmanFord(new Graph(map)), length);
+		testPathFinder(new Dijkstra(new Graph(map)), length);
+		testPathFinder(new AStar(new Graph(map), Heuristic.Euclidean), length);
+	}
+
+
+	@Test
+	public void testRandom() {
+		for (int i = 2; i < 50; ++i) {
+			for (int j = 2; j < 50; ++j) {
+				for (int k = 20; k < 40; ++k) {
+					testRandomGraph(i, j, k / 100);
+				}
+			}
+		}
+	}
+
+	private void testRandomGraph(int width, int height, double freq) {
+		char[][] map = generateRandom(width, height, freq);
+
+		LinkedList<Node> p1 = getShortestPath(new Dijkstra(new Graph(map)));
+		LinkedList<Node> p2 = getShortestPath(new AStar(new Graph(map), Heuristic.Euclidean));
+
+		assertEquals(p1.size(), p2.size());
+		assertEquals(getPathDistance(p1), getPathDistance(p2), 1E-9);
+
+		if (width < 20 && height < 20) {
+			LinkedList<Node> p3 = getShortestPath(new BellmanFord(new Graph(map)));
+
+			assertEquals(p2.size(), p3.size());
+			assertEquals(getPathDistance(p2), getPathDistance(p3), 1E-9);
+		}
+	}
 
 	public static char[][] generateRandom(int width, int height, double freq) {
 		char[][] graph = new char[height][width];
@@ -199,12 +318,21 @@ public class ShortestPathTest {
 
 		for (int i = 0; i < height; ++i) {
 			for (int j = 0; j < width; ++j) {
-				graph[i][j] = (rand.nextDouble() <= freq ? '#' : '.');
+				graph[i][j] = (rand.nextDouble() <= freq ? '#' : 'p');
 			}
 		}
 
-		graph[0][0] = 's';
-		graph[width - 1][height - 1] = 't';
+		graph[rand.nextInt(height)][rand.nextInt(width)] = 's';
+
+		while (true) {
+			int h = rand.nextInt(height);
+			int w = rand.nextInt(width);
+
+			if (graph[h][w] != 's') {
+				graph[h][w] = 't';
+				break;
+			}
+		}
 
 		return graph;
 	}
@@ -215,6 +343,15 @@ public class ShortestPathTest {
 		}
 
 		return path.get(path.size() - 1).getDistance();
+	}
+
+	public static LinkedList<Node> getShortestPath(PathFinder f) {
+		f.findPath();
+
+		LinkedList<Node> path = f.getShortestPath();
+		assertTrue(isValidPath(f.getGraph(), path));
+
+		return path;
 	}
 
 	public static boolean isValidPath(Graph graph, LinkedList<Node> path) {
